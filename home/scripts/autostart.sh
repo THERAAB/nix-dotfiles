de5de849bc5c675e6333f7bf343720ed10158c46
THERAAB <rob.w.lago@gmail.com>
2023-03-18 18:59:00 -0400

    automated commit

---
+++ home/scripts/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+maestral start &
+~/.config/scripts/headphone_lights.sh &
+~/.config/scripts/restart_openrazer_loop.sh &
+steam -silent &
--- nixos/default.nix
+++ nixos/default.nix
@@ -12,6 +12,5 @@
     ./hardware-configuration.nix
     ./sops.nix
     ./gnome.nix
-    ./autostart.nix
   ];
 }
