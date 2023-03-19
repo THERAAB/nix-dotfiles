d15759e80c3a56322e7de20b0bc34caf7b4a93b8
THERAAB <rob.w.lago@gmail.com>
2023-03-19 13:52:32 -0400

    automated commit

--- home/persist.nix
+++ home/persist.nix
@@ -19,6 +19,7 @@
       ".config/libreoffice"
       ".config/cef_user_data" # Steam
       ".config/JetBrains"
+      ".config/ulauncher"
       ".cache/nvim"
       ".cache/mozilla"
       ".cache/matplotlib" # Fonts
