aad9381aaea7ec91163ef893c79fa83690f1a8a5
THERAAB <rob.w.lago@gmail.com>
2023-03-19 19:24:03 -0400

    automated commit

--- home/default.nix
+++ home/default.nix
@@ -2,7 +2,7 @@
 {
   imports = [
     ./zsh
-    ./razer
+#    ./razer
     ./persist.nix
     ./pkgs.nix
     ./git.nix
--- home/razer/default.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-{
-  home.file.".config/openrazer/razer.conf" = {
-    source = ./razer.conf;
-  };
-}
