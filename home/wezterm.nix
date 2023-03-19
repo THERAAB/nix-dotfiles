9dfde4164dd6d4cbdff495562b08439c2e587a0d
THERAAB <rob.w.lago@gmail.com>
2023-03-19 08:35:58 -0400

    automated commit

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -4,13 +4,21 @@
     enable = true;
     extraConfig = ''
       return {
-        color_scheme = 'Edge Dark (base16)',
+        color_scheme = 'Nord (base16)',
         font = wezterm.font 'JetBrainsMono Nerd Font',
         window_background_opacity = 0.9,
         text_background_opacity = 0.9,
         colors = {
         },
         enable_tab_bar = false,
+        enable_wayland = true,
+        window_frame = {
+          active_titlebar_bg = '#2e3440',
+          inactive_titlebar_bg = '#2e3440',
+          inactive_titlebar_border_bottom = '#2e3440',
+          active_titlebar_border_bottom = '#2e3440',
+          button_bg = '#2e3440',
+        },
       }
     '';
   };
