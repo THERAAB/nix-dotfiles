d0347cae86615a6a9ca0654b4bacf16cb09ca8e2
THERAAB <rob.w.lago@gmail.com>
2023-03-21 07:59:48 -0400

    automated commit

--- home/maestral.nix
+++ home/maestral.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }:
 {
+  systemd.user.services.maestral = {
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      Type = "notify";
+      ExecStart = "${pkgs.maestral}/bin/maestral start -f";
+      Environment = "PYTHONOPTIMIZE=2 LC_CTYPE=UTF-8";
+    };
+  };
   home.packages = with pkgs; [
     maestral
   ];
