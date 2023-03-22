128ccfa5cf3efa1c01c23f366e3988a9c712f71f
THERAAB <rob.w.lago@gmail.com>
2023-03-22 14:27:31 -0400

    automated commit

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -14,13 +14,6 @@
         enable_tab_bar = false,
         window_decorations = "RESIZE",
         enable_wayland = true,
-        window_frame = {
-          active_titlebar_bg = '#2e3440',
-          inactive_titlebar_bg = '#2e3440',
-          inactive_titlebar_border_bottom = '#2e3440',
-          active_titlebar_border_bottom = '#2e3440',
-          button_bg = '#2e3440',
-        },
         window_padding = {
           left = '20px',
           right = '20px',
