90f089b4afee822c13f258f38aa3d2c6af14aa97
THERAAB <rob.w.lago@gmail.com>
2023-03-27 16:10:09 -0400

    automated commit

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -2,25 +2,25 @@
 {  
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    initrd.verbose = false;
-    plymouth.enable = true;
+#    initrd.verbose = false;
+#    plymouth.enable = true;
     loader.efi.canTouchEfiVariables = true;
     loader.grub = {
       enable = true;
       devices = ["nodev"];
       efiSupport = true;
-      configurationLimit = 10;
+#      configurationLimit = 10;
       gfxmodeEfi = "text";
       splashImage = null;
-      extraConfig = ''
-        if keystatus --shift ; then
-          set timeout=-1
-        else
-          set timeout=0
-        fi
-      '';
+#      extraConfig = ''
+#        if keystatus --shift ; then
+#          set timeout=-1
+#        else
+#          set timeout=0
+#        fi
+#      '';
     };
-    kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "udev.log_priority=3" "boot.shell_on_fail" ];
-    consoleLogLevel = 0;
+#    kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "udev.log_priority=3" "boot.shell_on_fail" ];
+#    consoleLogLevel = 0;
   };
 }
