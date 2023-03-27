63ebdd2ffc7f7e0dc916831b9638394612f177e4
THERAAB <rob.w.lago@gmail.com>
2023-03-27 19:18:13 -0400

    automated commit

--- nixos/fish.nix
+++ nixos/fish.nix
@@ -20,6 +20,21 @@
   programs.starship = {
     enable = true;
     settings = {
+      hostname = {
+        ssh_only = false;
+        format = "on [$hostname](bold yellow) ";
+      };
+      username = {
+        style_user = "blue bold";
+        style_root = "red bold";
+        format = "[$user]($style) ";
+        disabled = false;
+        show_always = true;
+      };
+      directory = {
+        format = "at [$path]($style)[$read_only]($read_only_style) ";
+        read_only = " 🔒";
+      };
       character = {
         success_symbol = "[➜](bold green)";
         error_symbol = "[☓](bold red)";
