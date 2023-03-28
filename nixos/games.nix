37ab0c4214931d24cb5ae2329b4fab98df70b78d
THERAAB <rob.w.lago@gmail.com>
2023-03-28 11:47:36 -0400

    automated commit

--- nixos/games.nix
+++ nixos/games.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 { 
   hardware.opengl.enable = true;
+  # Temporary workaround to change mesa version so GNOME Wayland can boot
+  # Can remove after below is updated:
+  # https://github.com/NixOS/nixpkgs/commits/nixos-unstable/nixos/modules/hardware/opengl.nix
+  hardware.opengl.mesaPackage = pkgs.mesa;
   programs.steam.enable = true;
   programs.gamemode = { 
     enable = true;
