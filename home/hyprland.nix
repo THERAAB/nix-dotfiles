7d12e6ca8da6dcd8893eddc4a7a3ef9274280952
THERAAB <rob.w.lago@gmail.com>
2023-04-22 09:22:11 -0400

    automated commit

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -4,6 +4,7 @@
     enable = true;
     extraConfig = ''
       bind = ALT, Return, exec, foot
+      bind = ALT, r, exec, ulauncer-toggle
       bind = ALT SHIFT, Q, killactive
       bind = ALT, 1, workspace, 1
       bind = ALT, 2, workspace, 2
