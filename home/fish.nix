f9832bd83b2a72d022efa07d8ab2d27fb1feb63e
THERAAB <rob.w.lago@gmail.com>
2023-05-23 07:20:16 -0400

    automated commit

--- home/fish.nix
+++ home/fish.nix
@@ -1,6 +1,23 @@
 {...}: {
   programs = {
-    fish.enable = true;
+    fish = {
+      enable = true;
+      shellAliases = {
+        update-channels = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_channel";
+        update-flake = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_flake";
+        update-full-with-git = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_full";
+        update-git-dotfiles = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_dotfiles_git";
+        garbage-collect-all = "/nix/persist/nix-dotfiles/nixos-update-manager.sh gc";
+        ls = "exa";
+        la = "exa -lah";
+        vi = "hx";
+        cat = "bat --theme=base16-256";
+        grep = "rg";
+        ps = "procs";
+        du = "dust";
+        htop = "btm";
+      };
+    };
     zoxide = {
       enable = true;
       options = ["--cmd cd"];
--- nixos/fish.nix
+++ nixos/fish.nix
@@ -6,20 +6,5 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
     '';
-    shellAliases = {
-      update-channels = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_channel";
-      update-flake = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_flake";
-      update-full-with-git = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_full";
-      update-git-dotfiles = "/nix/persist/nix-dotfiles/nixos-update-manager.sh update_dotfiles_git";
-      garbage-collect-all = "/nix/persist/nix-dotfiles/nixos-update-manager.sh gc";
-      ls = "exa";
-      la = "exa -lah";
-      vi = "hx";
-      cat = "bat --theme=base16-256";
-      grep = "rg";
-      ps = "procs";
-      du = "dust";
-      htop = "btm";
-    };
   };
 }
