dee066fc1a2a35a54e660c49f1cfff1e690750c0
THERAAB <rob.w.lago@gmail.com>
2023-05-29 08:17:03 -0400

    automated commit

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,5 +1,32 @@
-{...}: {
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
+    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      extraPolicies = {
+        CaptivePortal = false;
+        DisableFirefoxStudies = true;
+        DisableTelemetry = true;
+        DisableFirefoxAccounts = false;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        OfferToSaveLoginsDefault = false;
+        PasswordManagerEnabled = false;
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
+      };
+    };
+    profiles.default = {
+      id = 0;
+      isDefault = true;
+      name = "Default";
+      search = {
+        default = "DuckDuckGo";
+        force = true;
+      };
+      settings = {
+      };
+    };
   };
 }
