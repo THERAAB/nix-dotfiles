34b025d1877c63236123283e0742098ebcbb7694
THERAAB <rob.w.lago@gmail.com>
2023-06-20 19:43:49 -0400

    automated commit

--- hosts/nix-zenbook/home/gnome.nix
+++ hosts/nix-zenbook/home/gnome.nix
@@ -3,5 +3,11 @@
     "org/gnome/desktop/peripherals/touchpad" = {
       tap-to-click = true;
     };
+    "org/gnome/shell" = {
+      last-selected-power-profile = "power-saver";
+    };
+    "org/gnome/desktop/interface" = {
+      show-battery-percentage = true;
+    };
   };
 }
