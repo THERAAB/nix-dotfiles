7d7afd9e7a43b3f937131d74c5a5d1fc2bad4428
THERAAB <rob.w.lago@gmail.com>
2023-06-19 20:22:25 -0400

    automated commit

--- hosts/nix-zenbook/nixos/hardware.nix
+++ hosts/nix-zenbook/nixos/hardware.nix
@@ -1,8 +1,4 @@
 {...}: {
   hardware.cpu.intel.updateMicrocode = true;
   networking.hostName = "nix-zenbook";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 }
