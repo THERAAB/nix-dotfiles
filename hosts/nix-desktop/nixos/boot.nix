2e4f8191de8b7011462e25e169672a05631a136b
THERAAB <rob.w.lago@gmail.com>
2023-06-21 19:01:10 -0400

    automated commit

--- hosts/nix-desktop/nixos/boot.nix
+++ hosts/nix-desktop/nixos/boot.nix
@@ -1,8 +1,5 @@
 {...}: {
   boot = {
-    initrd.verbose = false;
-    kernelParams = ["quiet" "splash" "rd.systemd.show_status=false" "udev.log_priority=3" "boot.shell_on_fail"];
-    consoleLogLevel = 0;
     loader.grub.extraConfig = ''
       if keystatus --shift ; then
         set timeout=-1
--- share/nixos/boot.nix
+++ share/nixos/boot.nix
@@ -2,6 +2,9 @@
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     loader.efi.canTouchEfiVariables = true;
+    initrd.verbose = false;
+    consoleLogLevel = 0;
+    kernelParams = ["quiet" "splash" "rd.systemd.show_status=false" "udev.log_priority=3" "boot.shell_on_fail"];
     loader.grub = {
       enable = true;
       devices = ["nodev"];
