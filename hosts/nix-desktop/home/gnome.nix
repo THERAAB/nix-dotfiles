990d1a2b2ae4816b79e4e6fdde286e0741020004
THERAAB <rob.w.lago@gmail.com>
2023-06-21 20:09:08 -0400

    automated commit

--- hosts/nix-desktop/home/gnome.nix
+++ hosts/nix-desktop/home/gnome.nix
@@ -1,6 +1,8 @@
 {...}: {
-  "org/gnome/desktop/background" = {
-    picture-uri = "file:///nix/persist/nix-dotfiles/hosts/nix-desktop/assets/wall.jpg";
-    picture-uri-dark = "file:///nix/persist/nix-dotfiles/hosts/nix-desktop/assets/wall.jpg";
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      picture-uri = "file:///nix/persist/nix-dotfiles/hosts/nix-desktop/assets/wall.jpg";
+      picture-uri-dark = "file:///nix/persist/nix-dotfiles/hosts/nix-desktop/assets/wall.jpg";
+    };
   };
 }
