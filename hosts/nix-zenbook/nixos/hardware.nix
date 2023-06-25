0310db86df65cc1413f6f88a8b32564b2e7f2285
THERAAB <rob.w.lago@gmail.com>
2023-06-25 10:22:29 -0400

    automated commit

--- hosts/nix-zenbook/nixos/hardware.nix
+++ hosts/nix-zenbook/nixos/hardware.nix
@@ -13,5 +13,9 @@
     scsiLinkPolicy = "med_power_with_dipm";
     powertop.enable = true;
   };
-  services.thermald.enable = true;
+  services = {
+    thermald.enable = true;
+    logind.lidSwitch = "suspend-then-hibernate";
+  };
+  systemd.sleep.extraConfig = "HibernateDelaySec=8h";
 }
