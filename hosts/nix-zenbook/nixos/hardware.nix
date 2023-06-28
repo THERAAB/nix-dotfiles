c296c0bf64cd66e989b77ddb133ca9c0435a1ea6
THERAAB <rob.w.lago@gmail.com>
2023-06-27 20:09:36 -0400

    automated commit

--- hosts/nix-zenbook/nixos/hardware.nix
+++ hosts/nix-zenbook/nixos/hardware.nix
@@ -19,12 +19,12 @@
   networking.hostName = "nix-zenbook";
   powerManagement = {
     enable = true;
+    scsiLinkPolicy = "med_power_with_dipm";
+    powertop.enable = true;
   };
   services = {
     thermald.enable = true;
     logind.lidSwitch = "suspend-then-hibernate";
-    power-profiles-daemon.enable = false;
-    tlp.enable = true;
     # Disable ELAN Fingerprint reader
     udev.extraRules = ''ATTRS{idVendor}=="04f3", ATTRS{idProduct}=="0c6e", SUBSYSTEM=="usb", ATTR{authorized}="0"'';
   };
