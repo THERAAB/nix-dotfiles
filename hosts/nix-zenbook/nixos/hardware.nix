b453a4960a148e0661de031d777cb66d48fc5306
THERAAB <rob.w.lago@gmail.com>
2023-07-01 23:04:22 -0400

    automated commit

--- hosts/nix-zenbook/nixos/hardware.nix
+++ hosts/nix-zenbook/nixos/hardware.nix
@@ -31,20 +31,47 @@
     # Disable ELAN Fingerprint reader
     udev.extraRules = ''ATTRS{idVendor}=="04f3", ATTRS{idProduct}=="0c6e", SUBSYSTEM=="usb", ATTR{authorized}="0"'';
   };
-  systemd.sleep.extraConfig = ''
-    HibernateDelaySec=8h
-    HibernateMode=shutdown
-  '';
-  systemd.services.battery-charge-threshold = {
-    wantedBy = ["local-fs.target" "suspend.target"];
-    after = ["local-fs.target" "suspend.target"];
-    description = "Set the battery charge threshold%";
-    startLimitBurst = 5;
-    startLimitIntervalSec = 1;
-    serviceConfig = {
-      Type = "oneshot";
-      Restart = "on-failure";
-      ExecStart = "${pkgs.runtimeShell} -c 'echo 80 > /sys/class/power_supply/BAT?/charge_control_end_threshold'";
+  systemd = {
+    sleep.extraConfig = ''
+      HibernateDelaySec=8h
+      HibernateMode=shutdown
+    '';
+    services = {
+      battery-charge-threshold = {
+        wantedBy = ["local-fs.target" "suspend.target"];
+        after = ["local-fs.target" "suspend.target"];
+        description = "Set the battery charge threshold%";
+        startLimitBurst = 5;
+        startLimitIntervalSec = 1;
+        serviceConfig = {
+          Type = "oneshot";
+          Restart = "on-failure";
+          ExecStart = "${pkgs.runtimeShell} -c 'echo 80 > /sys/class/power_supply/BAT?/charge_control_end_threshold'";
+        };
+      };
+      keyboard-backlight = {
+        wantedBy = ["default.target"];
+        after = ["local-fs.target" "suspend.target"];
+        description = "Set the keyboard backlight";
+        startLimitBurst = 5;
+        startLimitIntervalSec = 1;
+        serviceConfig = {
+          Type = "oneshot";
+          Restart = "on-failure";
+          ExecStart = toString (pkgs.writeShellScript "keyboard-backlight" ''
+            H=$(date "+%k")
+            BACKLIGHT_ON=$((( 8 < H && H < 20 )) && echo 0 || echo 1)
+            echo $BACKLIGHT_ON > /sys/class/leds/asus::kbd_backlight/brightness
+          '');
+        };
+      };
+    };
+    timers.keyboard-backlight = {
+      timerConfig = {
+        Unit = "keyboard-backlight.service";
+        OnCalendar = "hourly";
+      };
+      wantedBy = ["timers.target"];
     };
   };
 }
