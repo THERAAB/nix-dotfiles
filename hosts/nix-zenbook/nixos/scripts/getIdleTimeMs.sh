deac1f2d6d20fb666ce6170fb5993faebf2b74d8
THERAAB <rob.w.lago@gmail.com>
2023-07-16 12:34:53 -0400

    automated commit

--- hosts/nix-zenbook/nixos/scripts/getIdleTimeMs.sh
+++ hosts/nix-zenbook/nixos/scripts/getIdleTimeMs.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
-
 let "div = 1000"
-let "limit = $1"
+let "limit = 60"
 dimmed=false
 cmd=/sys/class/leds/asus::kbd_backlight/brightness
 
-echo hello
-
 while true
 do
-  sleep 5
-  echo checking idle
+  sleep 1
   idle_string=`dbus-send --print-reply --dest=org.gnome.Mutter.IdleMonitor /org/gnome/Mutter/IdleMonitor/Core org.gnome.Mutter.IdleMonitor.GetIdletime | grep uint64`
   idle=`echo $idle_string | awk '{print $2}'`
-  echo $idle
   if [ $(($idle / $div)) -gt $limit ] && [ $dimmed == false ]; then
-    echo dimming
     sudo echo 0 | tee $cmd 
     dimmed=true
   elif [ $(($idle / $div)) -le $limit ] && [ $dimmed == true ]; then
-    echo undimming
-    sudo echo 3 | tee $cmd 
+    sudo echo 1 | tee $cmd 
     dimmed=false
   fi
 done
