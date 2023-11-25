06d9c56bed88954cee8cdb10b0cf2ab5ab6511b5
THERAAB <rob.w.lago@gmail.com>
2023-11-25 12:21:39 -0500

    enable webrtc

--- share/home/firefox.nix
+++ share/home/firefox.nix
@@ -38,7 +38,7 @@
         "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
         "geo.enabled" = false;
         "browser.search.suggest.enabled" = false;
-        "media.peerconnection.enabled" = false;
+        "media.peerconnection.enabled" = true;
       };
     };
   };
