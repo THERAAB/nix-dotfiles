f9c5ec318a3af253a86e2c809c78072f7e850499
THERAAB <rob.w.lago@gmail.com>
2023-11-26 07:46:14 -0500

    add syncthing

--- hosts/nix-zenbook/nixos/syncthing.nix
+++ hosts/nix-zenbook/nixos/syncthing.nix
@@ -1,8 +1,18 @@
 {...}: {
   services.syncthing = {
     enable = true;
+    user = "raab";
     openDefaultPorts = true;
     overrideDevices = true;
     overrideFolders = true;
+    devices = {
+      nix-server.id = "W33BOU2-KH5UGR6-MLWF3FP-EO4D4MT-QJZHZ44-XGSW54C-JXWMZFB-W5DKMQU";
+    };
+    folders = {
+      Share = {
+        path = "/home/raab/Documents";
+        devices = ["nix-server"];
+      };
+    };
   };
 }
