07cc139eb68a1cfc6ff6df977a1002e600e6720a
THERAAB <rob.w.lago@gmail.com>
2023-11-26 13:32:26 -0500

    syncthing

--- hosts/nix-desktop/nixos/syncthing.nix
+++ hosts/nix-desktop/nixos/syncthing.nix
@@ -16,11 +16,15 @@
         id = "H46DP2U-MISHKSS-EC64UUM-F65VNK4-QTQ2AHP-BO6CRLK-55OAZ2V-QWMGAQS";
         addresses = ["tcp://100.85.214.18:22000" "tcp://192.168.1.4:22000"];
       };
+      galaxy-tab = {
+        id = "LYRX4BK-OXATK4G-UKSLKED-RR6HO4L-VQ2P2U4-S6NTCPQ-OHZBKSV-NH24XQU";
+        addresses = ["tcp://192.168.1.36:22000"];
+      };
     };
     folders = {
       Share = {
         path = "/dropbox/share";
-        devices = ["nix-server" "nix-zenbook"];
+        devices = ["nix-server" "nix-zenbook" "galaxy-tab"];
         versioning = {
           type = "staggered";
           params.maxAge = "7776000"; # 90 days
--- hosts/nix-zenbook/nixos/syncthing.nix
+++ hosts/nix-zenbook/nixos/syncthing.nix
@@ -16,11 +16,15 @@
         id = "4M2BE3E-NS374SE-YFOEGQZ-KDOWHHK-SG4W5QG-TDKQPBI-RF3SYDP-PDMLVAJ";
         addresses = ["tcp://192.168.2.2:22000" "tcp://100.126.231.47:22000"];
       };
+      galaxy-tab = {
+        id = "LYRX4BK-OXATK4G-UKSLKED-RR6HO4L-VQ2P2U4-S6NTCPQ-OHZBKSV-NH24XQU";
+        addresses = ["tcp://192.168.1.36:22000"];
+      };
     };
     folders = {
       Share = {
         path = "/dropbox/share";
-        devices = ["nix-server" "nix-desktop"];
+        devices = ["nix-server" "nix-desktop" "galaxy-tab"];
         versioning = {
           type = "staggered";
           params.maxAge = "7776000"; # 90 days
