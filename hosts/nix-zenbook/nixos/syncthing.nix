014c6b1bc08a8f755edf05eaebb7d9a728f4b854
THERAAB <rob.w.lago@gmail.com>
2023-11-26 08:48:07 -0500

    update syncthing

--- hosts/nix-zenbook/nixos/syncthing.nix
+++ hosts/nix-zenbook/nixos/syncthing.nix
@@ -1,13 +1,17 @@
 {...}: {
   services.syncthing = {
     enable = true;
+    relay.enable = false;
     user = "raab";
     configDir = "/nix/persist/home/raab/.config/syncthing";
     openDefaultPorts = true;
     overrideDevices = true;
     overrideFolders = true;
     devices = {
-      nix-server.id = "F2373FD-HW5QAKL-NEXKV73-QFMLFZF-47SKVYQ-HHDCO6S-XOYOGTS-6PSKIAP";
+      nix-server = {
+        id = "F2373FD-HW5QAKL-NEXKV73-QFMLFZF-47SKVYQ-HHDCO6S-XOYOGTS-6PSKIAP";
+        addresses = ["tcp://192.168.3.2:22000" "tcp://100.121.108.123:22000"];
+      };
     };
     folders = {
       Share = {
