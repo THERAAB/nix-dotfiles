6acd7d874ed0ab3c61bf30628e1ddb351d4823bd
THERAAB <rob.w.lago@gmail.com>
2023-11-26 07:39:51 -0500

    add syncthing

--- hosts/nix-zenbook/nixos/syncthing.nix
+++ hosts/nix-zenbook/nixos/syncthing.nix
@@ -4,14 +4,5 @@
     openDefaultPorts = true;
     overrideDevices = true;
     overrideFolders = true;
-    devices = {
-      nix-zenbook.id = "M3OWV56-LFY5O5S-AYUOLEL-AOJN6FS-E3LA3XY-6QUG5MV-TIDRRNY-C3YS7AT";
-    };
-    folders = {
-      "Share" = {
-        path = "/home/raab/Documents";
-        devices = ["nix-zenbook"];
-      };
-    };
   };
 }
