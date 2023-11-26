53577811c9d4d9021b814964a4a36b4abbb05015
THERAAB <rob.w.lago@gmail.com>
2023-11-26 13:01:17 -0500

    syncthing

--- hosts/nix-zenbook/nixos/syncthing.nix
+++ hosts/nix-zenbook/nixos/syncthing.nix
@@ -12,11 +12,15 @@
         id = "F2373FD-HW5QAKL-NEXKV73-QFMLFZF-47SKVYQ-HHDCO6S-XOYOGTS-6PSKIAP";
         addresses = ["tcp://192.168.3.2:22000" "tcp://100.121.108.123:22000"];
       };
+      nix-desktop = {
+        id = "4M2BE3E-NS374SE-YFOEGQZ-KDOWHHK-SG4W5QG-TDKQPBI-RF3SYDP-PDMLVAJ";
+        addresses = ["tcp://192.168.2.2:22000" "tcp://100.126.231.47:22000"];
+      };
     };
     folders = {
       Dropbox = {
         path = "/dropbox";
-        devices = ["nix-server"];
+        devices = ["nix-server" "nix-desktop"];
         versioning = {
           type = "staggered";
           params.maxAge = "7776000"; # 90 days
