8876b4d64fa6a2c83f55f75f1d24d899e7d55a36
THERAAB <rob.w.lago@gmail.com>
2023-12-02 18:19:11 -0500

    update syncthing

--- share/nixos/syncthing.nix
+++ share/nixos/syncthing.nix
@@ -12,16 +12,15 @@
           id = "W33BOU2-KH5UGR6-MLWF3FP-EO4D4MT-QJZHZ44-XGSW54C-JXWMZFB-W5DKMQU";
           addresses = ["tcp://192.168.3.2:22000" "tcp://100.121.108.123:22000"];
           introducer = true;
+          autoAcceptFolders = true;
         };
       };
-      folders = {
-        "/dropbox/share" = {
-          id = "share";
-          devices = ["nix-server"];
-          versioning = {
-            type = "staggered";
-            params.maxAge = "7776000"; # 90 days
-          };
+      "/dropbox" = {
+        id = "default";
+        devices = ["nix-server"];
+        versioning = {
+          type = "staggered";
+          params.maxAge = "7776000"; # 90 days
         };
       };
     };
