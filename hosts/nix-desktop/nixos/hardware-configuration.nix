2f0fc5f2401cfa47865bf8ec4499882edbdb77d7
THERAAB <rob.w.lago@gmail.com>
2023-12-28 08:25:15 -0500

    automated commit

--- hosts/nix-desktop/nixos/hardware-configuration.nix
+++ hosts/nix-desktop/nixos/hardware-configuration.nix
@@ -8,47 +8,45 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
+  boot = {
+    initrd = {
+      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+      kernelModules = [];
+    };
+    kernelModules = ["kvm-amd"];
+  };
 
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
     options = ["size=10G" "mode=755"];
   };
-
   fileSystems."/home/raab" = {
     device = "none";
     fsType = "tmpfs";
     options = ["size=10G" "mode=777"];
   };
-
   fileSystems."/nix" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
     options = ["subvol=nix" "compress=zstd" "noatime"];
   };
-
   fileSystems."/nix/persist" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
     options = ["subvol=persist" "compress=zstd" "noatime"];
     neededForBoot = true;
   };
-
   fileSystems."/games" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
     options = ["subvol=games" "compress=zstd" "noatime"];
   };
-
   fileSystems."/dropbox" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
     options = ["subvol=dropbox" "compress=zstd" "noatime"];
   };
-
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/BOOT";
     fsType = "vfat";
