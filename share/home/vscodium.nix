7c80e651da7367bc195df492306a09100a42b820
THERAAB <rob.w.lago@gmail.com>
2024-01-24 18:18:40 -0500

    automated commit

--- share/home/vscodium.nix
+++ share/home/vscodium.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    # package = pkgs.vscodium;
     mutableExtensionsDir = false;
     userSettings = {
       "[nix]" = {
