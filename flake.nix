86506757d63356e4b784a2b0ded8738031b9c5f8
THERAAB <rob.w.lago@gmail.com>
2024-03-13 09:19:30 -0400

    automated commit

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -39,6 +39,5 @@
         impermanence.nixosModules.home-manager.impermanence
         ./share/home
       ];
-
     };
 }
