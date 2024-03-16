d43180eea50399c113f37350564253e1e96bd775
THERAAB <rob.w.lago@gmail.com>
2024-03-16 08:12:44 -0400

    moving to modules

---
+++ modules/nixos/workstation/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.nix-dotfiles; let
+  cfg = config.nix-dotfiles.workstation;
+in {
+  options.nix-dotfiles.workstation = with types; {
+    enable = mkEnableOption (lib.mdDoc "Setup workstation");
+  };
+  config = mkIf cfg.enable {
+    nix-dotfiles = {
+      workstation = {
+        boot.enable = true;
+        auto-upgrade.enable = true;
+        configuration.enable = true;
+        plymouth.enable = true;
+        fish.enable = true;
+        gnome.enable = true;
+        hardware.enable = true;
+        starship.enable = true;
+        syncthing.enable = true;
+        system.enable = true;
+        users.enable = true;
+      };
+      utils.nox.enable = true;
+    };
+    programs.firefox.enable = true;
+  };
+}
--- share/nixos/default.nix
+++ share/nixos/default.nix
@@ -1,23 +1,7 @@
 {...}: {
   imports = [
-    ./users.nix
     ./sops.nix
     ./persist.nix
   ];
-  nix-dotfiles = {
-    workstation = {
-      boot.enable = true;
-      auto-upgrade.enable = true;
-      configuration.enable = true;
-      plymouth.enable = true;
-      fish.enable = true;
-      gnome.enable = true;
-      hardware.enable = true;
-      starship.enable = true;
-      syncthing.enable = true;
-      system.enable = true;
-    };
-    utils.nox.enable = true;
-  };
-  programs.firefox.enable = true;
+  nix-dotfiles.workstation.enable = true;
 }
